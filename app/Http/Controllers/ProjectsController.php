7e73ed6cf1c58877140313bed6138c7e19d37905
Ricardo Montañana <rmontanana@gmail.com>
2018-12-22 20:48:14 +0100

    estético

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -10,16 +10,13 @@ class ProjectsController extends Controller
     public function index()
     {
         $projects = Project::all();
-
         return view('projects.index', compact('projects'));
     }
 
     public function store()
     {
         $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
-        //Persists
         Project::create($attributes);
-        //Redirect
         return redirect('projects');
     }
 }
